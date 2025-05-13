cbad8c08f949532ac3ff8b2867ffca7c668f0f77
Ricky Nandha <ricky.nandha2@gmail.com>
2025-05-13 13:41:40 +0100

    finall

---
+++ README.md
@@ -0,0 +1,10 @@
+assignment-07
+I can fill out a working form so that I can submit my information effectively and without issues.
+I can get the application to work as expected on my mobile device so that I can access and use it conveniently on the go.
+I created a working API GET route so that I can retrieve data from the server and display it to the users.
+I created a working API POST route so that users can send data to the server and store it.
+I created and seed a database with dummy data so that I have realistic test data for development purposes.
+
+I had issues with getting the form to clear once data has been submitted and the new data to instantly appear on the page however when the page refreshes the new data appears on the page and the form is cleared.
+
+I made the website to be visually impressive and user-friendly so that it provides a pleasant browsing experience.
---
+++ client/client/src/main.js
@@ -0,0 +1,61 @@
+console.log("Hello films!");
+
+const myFilmForm = document.getElementById("film-form");
+
+myFilmForm.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const data = new FormData(myFilmForm);
+  const formValues = Object.fromEntries(data);
+
+  console.log(formValues);
+
+  const serverResponse = await fetch("http://localhost:8080/film-reviews", {
+    method: "POST",
+    body: JSON.stringify(formValues),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const parsedResponse = await serverResponse.json();
+
+  console.log("The parsed servers response is: ", parsedResponse);
+
+  if (parsedResponse.success === true) {
+    myFilmForm.reset();
+
+    getAndRenderFilms();
+  }
+}
+const filmsContainer = document.getElementById("films-container");
+
+async function getAndRenderFilms() {
+  const response = await fetch("http://localhost:8080/film-reviews");
+  const data = await response.json();
+  console.log(data);
+
+  filmsContainer.innerHTML = "";
+
+  data.reverse().forEach(function (individualReview) {
+    const reviewDiv = document.createElement("div");
+    const rating = document.createElement("p");
+    const review = document.createElement("p");
+    const watchingExperience = document.createElement("p");
+    const title = document.createElement("h2");
+
+    const starCount = Number(individualReview.rating);
+    rating.innerHTML = "⭐".repeat(starCount);
+
+    review.textContent = individualReview.review;
+    watchingExperience.textContent = individualReview.watching_experience;
+    title.textContent = individualReview.title;
+
+    reviewDiv.append(title, rating, review, watchingExperience);
+    filmsContainer.appendChild(reviewDiv);
+  });
+}
+
+getAndRenderFilms();
